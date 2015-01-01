1429dd1aa2de26797f62a1b4648a9ff1e73ca7f2
theleastinterestingcoder <quanzhou64@gmail.com>
2014-12-31 20:25:02 -0500

    cleaned and commented code in movie_spider.py

--- movies/spiders/movie_spider.py
+++ movies/spiders/movie_spider.py
@@ -1,34 +1,54 @@
-import scrapy
+'''
+    movie_spider.py
+
+    written by: Quan Zhou
+    written on: December 24th, 2014
+
+    A python script to pull movie reviews from rotton tomatoes. 
+
+'''
+
+print(__doc__)
+
+
+import scrapy                                                   
+
+# movies.item is a class that you have to write
 from movies.items import MoviesItem
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors import LinkExtractor
+
+# Library for crawling rules
+from scrapy.contrib.spiders import CrawlSpider, Rule            
+from scrapy.contrib.linkextractors import LinkExtractor         
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 
 class MovieSpider(CrawlSpider):
-    name = "movie"
-    allowed_domains = ["www.rottentomatoes.com"]
+    name = "movie"                                      # Must be unique
+    allowed_domains = ["www.rottentomatoes.com"]        # Restrict spiders to a certain domain
     start_urls = [
-        "http://www.rottentomatoes.com/top/bestofrt/?year=2014",
+        "http://www.rottentomatoes.com/top/bestofrt/?year=2014",    # Where the first spider starts
     ]
 
+    # See readme.md for more on these rules (note: callback is set on our function 'parse_movie')
     rules = (
         Rule(SgmlLinkExtractor(allow=('http:\/\/www\.rottentomatoes\.com\/m\/\w+\/$', ), ), follow=True),
         Rule(SgmlLinkExtractor(allow=('\/m\/\w+\/reviews\/$', )), callback='parse_movie', follow=True),
         Rule(SgmlLinkExtractor(allow=('\/m\/\w+\/reviews\/\?page=\d+$', )), callback='parse_movie', follow=True),
     )
 
+    # This function scrapes information from a page
     def parse_movie(self, response):
+        # Assume our xpaths only work on our target page
         try:
             title = response.xpath('//div[@id="content"]//h2/text()').extract()
-            page = response.xpath('//span[@class="pageInfo"]/text()').extract()[0]
+            page  = response.xpath('//span[@class="pageInfo"]/text()').extract()[0]
         except:
             print "Error in URL: %s" % response.url
             return
 
-        reviews = set()
-
+        
         print 'Title: %s , %s' % (title, page)
+        reviews = set()
 
         # When we hit the appropriate page, try to scrape the review from each table row
         for sel in response.xpath('//div[@id="reviews"]//table//tr'):
@@ -43,12 +63,11 @@ class MovieSpider(CrawlSpider):
 
             reviews.add((rating, review))
             
-
-            
-
+        # Push the scraped data into the datastructure we've written
         item = MoviesItem()
         item['title'] = title
         item['reviews'] = list(reviews)
         item['page'] = page
+        
         yield item
 
